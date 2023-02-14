7a3418b8d54a4029df3313ebe9338a57611e2055
Adam Jemielita <trashci18@gmail.com>
2023-02-14 22:41:49 +0100

    trigram data prep

--- makemore/trigram_nn.py
+++ makemore/trigram_nn.py
@@ -1,2 +1,38 @@
-words = open('names.txt', 'r')
-print(words)
+import torch
+import matplotlib.pyplot as plt
+
+words = open('names.txt', 'r').read().splitlines()[:50]
+
+letters = sorted(list(set(''.join(words))))
+words = ('.' + word + '.' for word in words)
+trigrams = [(word[i:i+2], word[i+2]) for word in words for i in range(len(word)) if i+2 < len(word)]
+letter_pairs = set(tup[0] for tup in trigrams)
+
+str_to_inx_pairs = {str:inx+1 for inx, str in enumerate(letter_pairs)}
+str_to_inx_letter = {str:inx+1 for inx, str in enumerate(letters)}
+str_to_inx_pairs['.'] = 0
+str_to_inx_letter['.'] = 0
+inx_to_str_pairs = {inx:str for str, inx in str_to_inx_pairs.items()}
+inx_to_str_letter = {inx:str for str, inx in str_to_inx_letter.items()}
+
+N = torch.zeros(28,len(letter_pairs)+1, dtype=torch.int32)
+for trigram in trigrams:
+    j = str_to_inx_pairs[trigram[0]]
+    i = str_to_inx_letter[trigram[1]]
+    N[i, j] += 1
+
+
+plt.figure(figsize=(16,16))
+plt.imshow(N, cmap='Blues')
+for i in range(27):
+    for j in range(27):
+        chstr = inx_to_str_pairs[i] + inx_to_str_letter[j]
+        plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
+        plt.text(j, i, N[i, j].item(), ha="center", va="top", color='gray')
+plt.axis('off')
+
+
+print(N)
+# plt.imshow(N)
+
+plt.show()
